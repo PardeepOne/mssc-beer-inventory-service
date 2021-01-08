52ed92b853fc594e2541c29ed6f5a282b322af48
Pardeep <pardeeps460@gmail.com>
2021-01-08 23:23:37 +0100

    Configure Spring Profile for Local MySQL. closes #4

--- src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
+++ src/main/java/guru/sfg/beer/inventory/service/BeerInventoryApplication.java
@@ -2,11 +2,14 @@ package guru.sfg.beer.inventory.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
+import org.springframework.core.env.AbstractEnvironment;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ArtemisAutoConfiguration.class)
 public class BeerInventoryApplication {
 
     public static void main(String[] args) {
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME,"localmysql");
         SpringApplication.run(BeerInventoryApplication.class, args);
     }
 
